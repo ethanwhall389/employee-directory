a47bd0586bbd0eac590aebd1f02ecf0f46ed267b
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2024-04-11 20:43:59 +0000

    Add employee cards

---
+++ src/components/EmployeeCard.jsx
@@ -0,0 +1,13 @@
+export default function EmployeeCard({userData}) {
+    return (
+    <div className="border-box border-2 rounded-md w-96 flex gap-2 h-36 items-center p-4">
+                <img className="rounded-full h-full w-auto" src={userData.image} alt={`${userData.firstName} profile photo`} />
+            <div className="">
+                <p className="text-lg font-bold">{userData.firstName} {userData.lastName}</p>
+                <p>{userData.company.title} | {userData.company.department}</p>
+                <p>{userData.email}</p>
+                <p>{userData.phone}</p>
+            </div>
+    </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,25 @@
 import { useState, useEffect } from "react";
 import fetchData from "../utils/fetchData";
 import { dummyData } from "../utils/fetchData";
+import EmployeeCard from "../components/EmployeeCard";
 
 export default function Home() {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        // fetchData('https://dummyjson.com/users', setUsers)
-        setUsers(dummyData);
+        fetchData('https://dummyjson.com/users', setUsers)
+        // setUsers(dummyData);
     }, [])
 
 
     return (
         <>
         <h1>Employee Directory</h1>
-        {users.map((user) => (
-            <p key={user.id}>{user.firstName}</p>
-        ))}
+        <div className="grid grid-cols-1 grid-flow-dense gap-3 md:grid-cols-2 lg:grid-cols-3 place-items-center">
+            {users.map((user) => (
+                <EmployeeCard key={user.id} userData={user}/>
+            ))}
+        </div>
         </>
     )
 }

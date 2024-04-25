188ab8188cd653c4e18f423c25d5c327eda1eceb
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2024-04-25 16:46:37 +0000

    Add cursor hover pointer to GroupCard

--- src/components/GroupCard.jsx
+++ src/components/GroupCard.jsx
@@ -10,7 +10,7 @@ export default function GroupCard({groupData}) {
     }
 
     return (
-        <div className="border-box border-2 rounded-md w-full flex justify-between gap-2 h-36 items-center p-6 overflow-hidden" onClick={handleClick}>
+        <div className="border-box border-2 rounded-md w-full flex justify-between gap-2 h-36 items-center p-6 overflow-hidden hover:cursor-pointer" onClick={handleClick}>
             <div className='flex items-center gap-4'>
                 <GroupsIcon fontSize='large'/>
                 <h1>{groupData.groupName}</h1>
